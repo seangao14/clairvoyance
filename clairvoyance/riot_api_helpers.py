d756900268b5b6111fbba9f19ebfa9c5ed008230
seangao14 <y_gao@umail.ucsb.edu>
2020-11-06 18:17:45 -0800

    initial commit

---
+++ clairvoyance/riot_api_helpers.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import urllib.parse
+
+def url_to_json(url):
+    return requests.get(url).json()
+
+def get_summoner(api_key, arg, region='na1'):
+    '''
+    @arg summoners name
+    returns a json
+    '''
+    url = f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{urllib.parse.quote(arg)}?api_key={api_key}'
+    return url_to_json(url)
+
+def get_matchlist(api_key, arg, region='na1'):
+    '''
+    @arg takes accountId
+    returns a json
+    '''
+    url = f'https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/{urllib.parse.quote(arg)}?api_key={api_key}'
+    return url_to_json(url)
+
+def get_timeline(api_key, arg, region='na1'):
+    '''
+    @arg takes matchId
+    returns big json :)
+    '''
+    url = f'https://{region}.api.riotgames.com/lol/match/v4/timelines/by-match/{urllib.parse.quote(arg)}?api_key={api_key}'
+    return url_to_json(url)
+
+def get_challengers(api_key, region='na1'):
+    url = f'https://{region}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={api_key}'
+    return url_to_json(url)
