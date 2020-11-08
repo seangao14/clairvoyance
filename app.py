ca8f7f87a94d192c60f44fe50d6cf56ea8ce2895
alphro <diecypt@gmail.com>
2020-11-07 21:01:54 -0800

    updated front end

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ nav = Navigation(app)
 nav.Bar('main', [
     nav.Item('Home', 'index'),
     nav.Item('Calculator', 'calculator'),
+    nav.Item('Match History','match_history')
 ])
 
 @app.route('/', methods=['POST', 'GET'])
@@ -16,6 +17,10 @@ def index():
 @app.route('/calculator', methods=['POST', 'GET'])
 def calculator():
     return render_template('Calculator.html')
+
+@app.route('/match_history', methods=['POST', 'GET'])
+def match_history():
+    return render_template('Match_history.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
