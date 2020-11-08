0f9e96c95089e02b81ea2f0869b96ddc65c0e557
Elyse Yao <elyseyao@gmail.com>
2020-11-07 17:37:25 -0800

    Working navbar and tweaked container padding

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_navigation import Navigation
 
 app = Flask(__name__)
+nav = Navigation(app)
+
+nav.Bar('main', [
+    nav.Item('Home', 'index'),
+    nav.Item('Calculator', 'calculator'),
+])
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return render_template('HomePage.html')
+
+@app.route('/calculator', methods=['POST', 'GET'])
+def calculator():
     return render_template('Calculator.html')
 
 if __name__ == "__main__":
