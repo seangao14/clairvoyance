80e5c3c94740aeb4592cba7286a7d9aeac79cd74
Elyse Yao <elyseyao@gmail.com>
2020-11-07 21:34:27 -0800

    Parse POST from home search

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_navigation import Navigation
 
 app = Flask(__name__)
@@ -6,16 +6,28 @@ nav = Navigation(app)
 
 nav.Bar('main', [
     nav.Item('Home', 'index'),
-    nav.Item('Calculator', 'calculator'),
+    nav.Item('Calculator', 'calculator')
 ])
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        print("hello")
+        user = request.form['user']
+        return redirect(url_for('match_history', summoner_name = user))
     return render_template('home.html')
+
+@app.route('/matches/<summoner_name>')
+def match_history(summoner_name):
+    return render_template('match_history.html', summoner_name = summoner_name)
 
 @app.route('/calculator', methods=['POST', 'GET'])
 def calculator():
     return render_template('calculator.html')
+
+# @app.route('/match_history', methods=['POST', 'GET'])
+# def match_history():
+#     return render_template('match_history.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
