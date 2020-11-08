2270ac1ce5fdc6f05078d861b4759d7e66dd6320
Elyse Yao <elyseyao@gmail.com>
2020-11-08 02:27:55 -0800

    Match history not scuffed no more

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def index():
 
 @app.route('/matches/<summoner_name>')
 def match_history(summoner_name):
-    print(summoner_name)
     summoner = get_account_data(summoner_name)
     if summoner == -1:
         return render_template('404.html', name = summoner_name)
