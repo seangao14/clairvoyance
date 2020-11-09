c37e622e9a8b988101008f929feef73c18c8fc4f
seangao14 <y_gao@umail.ucsb.edu>
2020-11-08 22:54:56 -0800

    added exception handlers

--- app.py
+++ app.py
@@ -22,21 +22,27 @@ def index():
 
 @app.route('/matches/<summoner_name>')
 def match_history(summoner_name):
-    summoner = get_account_data(summoner_name)
-    if summoner == -1:
-        return render_template('404.html', name = summoner_name)
-    return render_template('match_history.html', summoner = summoner)
+    try:
+        summoner = get_account_data(summoner_name)
+        if summoner == -1:
+            return render_template('404.html', name = summoner_name)
+        return render_template('match_history.html', summoner = summoner)
+    except:
+        return 'API key error!'
 
 @app.route('/graph/<match_id>')
 def graph(match_id):
-    frames = get_frames(match_id)
-    if frames == -1:
-        return render_template('404.html', game = match_id)
-    game = get_game_data(match_id)
-    pred = predict(game)
-    gd = list(np.array(get_gd(game)) + 0.5)
-    xpd = list(np.array(get_xpd(game)) + 0.5)
-    return render_template('graph.html', match=match_id, frames=frames, pred=pred, gd=gd, xpd=xpd)
+    try:
+        frames = get_frames(match_id)
+        if frames == -1:
+            return render_template('404.html', game = match_id)
+        game = get_game_data(match_id)
+        pred = predict(game)
+        gd = list(np.array(get_gd(game)) + 0.5)
+        xpd = list(np.array(get_xpd(game)) + 0.5)
+        return render_template('graph.html', match=match_id, frames=frames, pred=pred, gd=gd, xpd=xpd)
+    except:
+        return 'API key error!'
 
 @app.route('/calculator/<match_id>/<frame_id>', methods=['POST', 'GET'])
 def custom_calculator(match_id, frame_id):
@@ -62,50 +68,53 @@ def custom_calculator(match_id, frame_id):
     #     'elder': '2'
     # }
     if request.method == 'POST':
-        data = request.form
-        champions = [data['b1'], data['b2'], data['b3'], data['b4'], data['b5'],
-                     data['r1'], data['r2'], data['r3'], data['r4'], data['r5']]
-        
-        b_levels = [float(data['b1l']), float(data['b2l']), float(data['b3l']), float(data['b4l']), float(data['b5l']), ]
-        r_levels = [float(data['r1l']), float(data['r2l']), float(data['r3l']), float(data['r4l']), float(data['r5l']), ]
+        try:
+            data = request.form
+            champions = [data['b1'], data['b2'], data['b3'], data['b4'], data['b5'],
+                        data['r1'], data['r2'], data['r3'], data['r4'], data['r5']]
+            
+            b_levels = [float(data['b1l']), float(data['b2l']), float(data['b3l']), float(data['b4l']), float(data['b5l']), ]
+            r_levels = [float(data['r1l']), float(data['r2l']), float(data['r3l']), float(data['r4l']), float(data['r5l']), ]
 
-        bg = float(data['b_gold'])
-        rg = float(data['r_gold'])
+            bg = float(data['b_gold'])
+            rg = float(data['r_gold'])
 
-        bk = float(data['b_kills'])
-        rk = float(data['r_kills'])
+            bk = float(data['b_kills'])
+            rk = float(data['r_kills'])
 
-        bt = float(data['b_towers'])
-        rt = float(data['r_towers'])
+            bt = float(data['b_towers'])
+            rt = float(data['r_towers'])
 
-        bi = float(data['b_inhibs'])
-        ri = float(data['r_inhibs'])
+            bi = float(data['b_inhibs'])
+            ri = float(data['r_inhibs'])
 
-        # for elder and baron:
-        # 0 = blue, 1 = red, 2 = neither
-        b_baron = 1 if int(data['baron']) == 0 else 0
-        r_baron = 1 if int(data['baron']) == 1 else 0
+            # for elder and baron:
+            # 0 = blue, 1 = red, 2 = neither
+            b_baron = 1 if int(data['baron']) == 0 else 0
+            r_baron = 1 if int(data['baron']) == 1 else 0
 
-        b_elder = 1 if int(data['elder']) == 0 else 0
-        r_elder = 1 if int(data['elder']) == 1 else 0
+            b_elder = 1 if int(data['elder']) == 0 else 0
+            r_elder = 1 if int(data['elder']) == 1 else 0
 
-        bm = [int(data['b_air']), int(data['b_earth']), int(data['b_fire']), int(data['b_water']), 
-                b_elder, int(data['b_herald']), b_baron]
-        
-        rm = [int(data['r_air']), int(data['r_earth']), int(data['r_fire']), int(data['r_water']), 
-                r_elder, int(data['r_herald']), r_baron]
-        
-        game = custom_game(timestamp=0.5, champions=champions, 
-            blue_gold=bg, red_gold=rg, blue_levels=b_levels, red_levels=r_levels,
-            bk=bk, rk=rk, bt=bt, rt=rt, bi=bi, ri=ri, bm=bm, rm=rm)
-        # print(game)
-        # print(len(game))
-        pred = predict(game)
+            bm = [int(data['b_air']), int(data['b_earth']), int(data['b_fire']), int(data['b_water']), 
+                    b_elder, int(data['b_herald']), b_baron]
+            
+            rm = [int(data['r_air']), int(data['r_earth']), int(data['r_fire']), int(data['r_water']), 
+                    r_elder, int(data['r_herald']), r_baron]
+            
+            game = custom_game(timestamp=0.5, champions=champions, 
+                blue_gold=bg, red_gold=rg, blue_levels=b_levels, red_levels=r_levels,
+                bk=bk, rk=rk, bt=bt, rt=rt, bi=bi, ri=ri, bm=bm, rm=rm)
+            # print(game)
+            # print(len(game))
+            pred = predict(game)
 
-        # print(data)
-        # print(type(data))
+            # print(data)
+            # print(type(data))
 
-        return render_template('calculator.html', data=data, pred=pred)
+            return render_template('calculator.html', data=data, pred=pred)
+        except Exception as e:
+            return f'Something went wrong, please go back and check your inputs.\n Error: {e}'
     return render_template('calculator.html', data=data)
 
 @app.route('/calculator', methods=['POST', 'GET'])
@@ -146,50 +155,53 @@ def calculator():
         'elder': '2'
     }
     if request.method == 'POST':
-        data = request.form
-        champions = [data['b1'], data['b2'], data['b3'], data['b4'], data['b5'],
-                     data['r1'], data['r2'], data['r3'], data['r4'], data['r5']]
-        
-        b_levels = [int(data['b1l']), int(data['b2l']), int(data['b3l']), int(data['b4l']), int(data['b5l']), ]
-        r_levels = [int(data['r1l']), int(data['r2l']), int(data['r3l']), int(data['r4l']), int(data['r5l']), ]
+        try:
+            data = request.form
+            champions = [data['b1'], data['b2'], data['b3'], data['b4'], data['b5'],
+                        data['r1'], data['r2'], data['r3'], data['r4'], data['r5']]
+            
+            b_levels = [int(data['b1l']), int(data['b2l']), int(data['b3l']), int(data['b4l']), int(data['b5l']), ]
+            r_levels = [int(data['r1l']), int(data['r2l']), int(data['r3l']), int(data['r4l']), int(data['r5l']), ]
 
-        bg = int(data['b_gold'])
-        rg = int(data['r_gold'])
+            bg = int(data['b_gold'])
+            rg = int(data['r_gold'])
 
-        bk = int(data['b_kills'])
-        rk = int(data['r_kills'])
+            bk = int(data['b_kills'])
+            rk = int(data['r_kills'])
 
-        bt = int(data['b_towers'])
-        rt = int(data['r_towers'])
+            bt = int(data['b_towers'])
+            rt = int(data['r_towers'])
 
-        bi = int(data['b_inhibs'])
-        ri = int(data['r_inhibs'])
+            bi = int(data['b_inhibs'])
+            ri = int(data['r_inhibs'])
 
-        # for elder and baron:
-        # 0 = blue, 1 = red, 2 = neither
-        b_baron = 1 if int(data['baron']) == 0 else 0
-        r_baron = 1 if int(data['baron']) == 1 else 0
+            # for elder and baron:
+            # 0 = blue, 1 = red, 2 = neither
+            b_baron = 1 if int(data['baron']) == 0 else 0
+            r_baron = 1 if int(data['baron']) == 1 else 0
 
-        b_elder = 1 if int(data['elder']) == 0 else 0
-        r_elder = 1 if int(data['elder']) == 1 else 0
+            b_elder = 1 if int(data['elder']) == 0 else 0
+            r_elder = 1 if int(data['elder']) == 1 else 0
 
-        bm = [int(data['b_air']), int(data['b_earth']), int(data['b_fire']), int(data['b_water']), 
-                b_elder, int(data['b_herald']), b_baron]
-        
-        rm = [int(data['r_air']), int(data['r_earth']), int(data['r_fire']), int(data['r_water']), 
-                r_elder, int(data['r_herald']), r_baron]
-        
-        game = custom_game(timestamp=0.5, champions=champions, 
-            blue_gold=bg, red_gold=rg, blue_levels=b_levels, red_levels=r_levels,
-            bk=bk, rk=rk, bt=bt, rt=rt, bi=bi, ri=ri, bm=bm, rm=rm)
-        # print(game)
-        # print(len(game))
-        pred = predict(game)
+            bm = [int(data['b_air']), int(data['b_earth']), int(data['b_fire']), int(data['b_water']), 
+                    b_elder, int(data['b_herald']), b_baron]
+            
+            rm = [int(data['r_air']), int(data['r_earth']), int(data['r_fire']), int(data['r_water']), 
+                    r_elder, int(data['r_herald']), r_baron]
+            
+            game = custom_game(timestamp=0.5, champions=champions, 
+                blue_gold=bg, red_gold=rg, blue_levels=b_levels, red_levels=r_levels,
+                bk=bk, rk=rk, bt=bt, rt=rt, bi=bi, ri=ri, bm=bm, rm=rm)
+            # print(game)
+            # print(len(game))
+            pred = predict(game)
 
-        # print(data)
-        # print(type(data))
+            # print(data)
+            # print(type(data))
 
-        return render_template('calculator.html', data=data, pred=pred)
+            return render_template('calculator.html', data=data, pred=pred)
+        except Exception as e:
+            return f'Something went wrong, please go back and check your inputs.\n Error: {e}'
     return render_template('calculator.html', data=data)
 
 # @app.route('/match_history', methods=['POST', 'GET'])
